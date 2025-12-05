20226aeebb6db2600b8526f24a5950bd90210f0c
LilLongg <glivingstone70@gmail.com>
2025-12-05 08:51:49 +0700

    Remove pointers from base class

--- include/autoencoder.h
+++ include/autoencoder.h
@@ -8,145 +8,101 @@
  * @brief Interface class that encapsulates the network
  *
  */
-class IAutoencoder { protected:
-    // The first conv2d layer of the encoder
-    static constexpr int _ENCODER_FILTER_1_DEPTH = 256;
-    static constexpr int _ENCODER_FILTER_1_SIZE = CONV_FILTER_WIDTH * CONV_FILTER_HEIGHT * IMAGE_DEPTH * _ENCODER_FILTER_1_DEPTH;
+class IAutoencoder {
+protected:
+  // The first conv2d layer of the encoder
+  static constexpr int _ENCODER_FILTER_1_DEPTH = 256;
+  static constexpr int _ENCODER_FILTER_1_SIZE =
+      CONV_FILTER_WIDTH * CONV_FILTER_HEIGHT * IMAGE_DEPTH * _ENCODER_FILTER_1_DEPTH;
 
-    unique_ptr<float[]> _encoder_filter_1;
-    unique_ptr<float[]> _encoder_bias_1;
+  // The second conv2d layer of the encoder
+  static constexpr int _ENCODER_FILTER_2_DEPTH = 128;
+  static constexpr int _ENCODER_FILTER_2_SIZE  = CONV_FILTER_WIDTH *
+                                                CONV_FILTER_HEIGHT *
+                                                _ENCODER_FILTER_1_DEPTH *
+                                                _ENCODER_FILTER_2_DEPTH;
 
-    // Save output for backwards propogation
-    unique_ptr<float[]> _out_encoder_filter_1;
-    unique_ptr<float[]> _out_encoder_bias_1;
+  // The first conv2d layer of the decoder
+  static constexpr int _DECODER_FILTER_1_DEPTH = 128;
+  static constexpr int _DECODER_FILTER_1_SIZE  = CONV_FILTER_WIDTH *
+                                                CONV_FILTER_HEIGHT *
+                                                _ENCODER_FILTER_2_DEPTH *
+                                                _DECODER_FILTER_1_DEPTH;
 
-    unique_ptr<float[]> _out_encoder_relu_1;
-    unique_ptr<float[]> _out_max_pooling_1;
+  // The second conv2d layer of the decoder
+  static constexpr int _DECODER_FILTER_2_DEPTH = 256;
+  static constexpr int _DECODER_FILTER_2_SIZE  = CONV_FILTER_WIDTH *
+                                                CONV_FILTER_HEIGHT *
+                                                _DECODER_FILTER_1_DEPTH *
+                                                _DECODER_FILTER_2_DEPTH;
 
-    // The second conv2d layer of the encoder
-    static constexpr int _ENCODER_FILTER_2_DEPTH = 128;
-    static constexpr int _ENCODER_FILTER_2_SIZE  = CONV_FILTER_WIDTH * CONV_FILTER_HEIGHT * _ENCODER_FILTER_1_DEPTH * _ENCODER_FILTER_2_DEPTH;
+  // The third conv2d layer of the decoder
+  static constexpr int _DECODER_FILTER_3_DEPTH = IMAGE_DEPTH;
+  static constexpr int _DECODER_FILTER_3_SIZE  = CONV_FILTER_WIDTH *
+                                                CONV_FILTER_HEIGHT *
+                                                _DECODER_FILTER_2_DEPTH *
+                                                _DECODER_FILTER_3_DEPTH;
 
-    unique_ptr<float[]> _encoder_filter_2;
-    unique_ptr<float[]> _encoder_bias_2;
-    unique_ptr<float[]> _out_encoder_filter_2;
-    unique_ptr<float[]> _out_encoder_bias_2;
+  // Encode and decode by batches to use less memory
+  static constexpr int _ENCODE_BATCH_SIZE = 1024;
 
-    unique_ptr<float[]> _out_encoder_relu_2;
-    unique_ptr<float[]> _out_max_pooling_2;
+public:
+  /**
+   * @brief Destroy the Autoencoder object
+   *
+   */
+  virtual ~IAutoencoder() = default;
 
-    // The first conv2d layer of the decoder
-    static constexpr int _DECODER_FILTER_1_DEPTH = 128;
-    static constexpr int _DECODER_FILTER_1_SIZE  = CONV_FILTER_WIDTH * CONV_FILTER_HEIGHT * _ENCODER_FILTER_2_DEPTH * _DECODER_FILTER_1_DEPTH;
+  /**
+   * @brief Encodes a dataset
+   *
+   * @param dataset The dataset to be encoded
+   * @return Dataset The encoded dataset
+   */
+  virtual Dataset encode(const Dataset &dataset) const = 0;
 
-    unique_ptr<float[]> _decoder_filter_1;
-    unique_ptr<float[]> _decoder_bias_1;
-    unique_ptr<float[]> _out_decoder_filter_1;
-    unique_ptr<float[]> _out_decoder_bias_1;
+  /**
+   * @brief Decodes a dataset
+   *
+   * @param dataset The dataset to be decoded
+   * @return Dataset The decoded dataset
+   */
+  virtual Dataset decode(const Dataset &dataset) const = 0;
 
-    unique_ptr<float[]> _out_decoder_relu_1;
-    unique_ptr<float[]> _out_upsampling_1;
+  /**
+   * @brief Trains the model using a dataset
+   *
+   * @param dataset The dataset
+   * @param n_epoch The number of epochs
+   * @param batch_size Minibatch size (set to 0 to disable)
+   * @param learning_rate Learning rate of the model
+   * @param verbose Whether to disable more or less information
+   * @param checkpoint Save the model's parameter after a specific number of epochs (set
+   * to 0 to disable)
+   * @param output_dir The file to save model's param
+   */
+  virtual void fit(const Dataset &dataset,
+                   int            n_epoch,
+                   int            batch_size,
+                   float          learning_rate,
+                   bool           verbose,
+                   int            checkpoint,
+                   const char    *output_dir = "./model") = 0;
 
-    // The second conv2d layer of the decoder
-    static constexpr int _DECODER_FILTER_2_DEPTH = 256;
-    static constexpr int _DECODER_FILTER_2_SIZE  = CONV_FILTER_WIDTH * CONV_FILTER_HEIGHT * _DECODER_FILTER_1_DEPTH * _DECODER_FILTER_2_DEPTH;
+  /**
+   * @brief Evaluate the model
+   *
+   * @param dataset The dataset to be evaluated
+   * @return float The MSE between the actual and expected result
+   */
+  virtual float eval(const Dataset &dataset) = 0;
 
-    unique_ptr<float[]> _decoder_filter_2;
-    unique_ptr<float[]> _decoder_bias_2;
-    unique_ptr<float[]> _out_decoder_filter_2;
-    unique_ptr<float[]> _out_decoder_bias_2;
-
-    unique_ptr<float[]> _out_decoder_relu_2;
-    unique_ptr<float[]> _out_upsampling_2;
-
-    // The third conv2d layer of the decoder
-    static constexpr int _DECODER_FILTER_3_DEPTH = IMAGE_DEPTH;
-    static constexpr int _DECODER_FILTER_3_SIZE  = CONV_FILTER_WIDTH * CONV_FILTER_HEIGHT * _DECODER_FILTER_2_DEPTH * _DECODER_FILTER_3_DEPTH;
-
-    unique_ptr<float[]> _decoder_filter_3;
-    unique_ptr<float[]> _decoder_bias_3;
-    unique_ptr<float[]> _out_decoder_filter_3;
-    unique_ptr<float[]> _out_decoder_bias_3;
-
-    // Gradients
-    unique_ptr<float[]> _d_in;
-    unique_ptr<float[]> _d_out;
-    unique_ptr<float[]> _d_filter;
-
-    // Encode and decode by batches to use less memory
-    static constexpr int _ENCODE_BATCH_SIZE = 1024;
-
-    /**
-     * @brief Allocate memory for the parameters
-     *
-     */
-    void _allocate_mem();
-
-    /**
-     * @brief Write the model's parameters to a file
-     *
-     * @param filename The file to write the model's parameter
-     */
-    void _save_paramters(const char *filename) const;
-
-  public:
-    /**
-     * @brief Initialize a new autoencoder with random paramters
-     *
-     */
-    IAutoencoder();
-
-    /**
-     * @brief Reads the parameters from a file
-     *
-     * @param filename The file containing the model's parameters
-     */
-    IAutoencoder(const char *filename);
-
-    /**
-     * @brief Destroy the Autoencoder object
-     *
-     */
-    virtual ~IAutoencoder() = default;
-
-    /**
-     * @brief Encodes a dataset
-     *
-     * @param dataset The dataset to be encoded
-     * @return Dataset The encoded dataset
-     */
-    virtual Dataset encode(const Dataset &dataset) const = 0;
-    
-    /**
-     * @brief Decodes a dataset
-     *
-     * @param dataset The dataset to be decoded
-     * @return Dataset The decoded dataset
-     */
-    virtual Dataset decode(const Dataset &dataset) const = 0;
-
-    /**
-     * @brief Trains the model using a dataset
-     *
-     * @param dataset The dataset
-     * @param n_epoch The number of epochs
-     * @param batch_size Minibatch size (set to 0 to disable)
-     * @param learning_rate Learning rate of the model
-     * @param verbose Whether to disable more or less information
-     * @param checkpoint Save the model's parameter after a specific number of epochs (set
-     * to 0 to disable)
-     * @param output_dir The file to save model's param
-     */
-    virtual void fit(const Dataset &dataset, int n_epoch, int batch_size, float learning_rate,
-                    bool verbose, int checkpoint, const char *output_dir = "./model") = 0;
-
-    /**
-     * @brief Evaluate the model
-     *
-     * @param dataset The dataset to be evaluated
-     * @return float The MSE between the actual and expected result
-     */
-    virtual float eval(const Dataset &dataset) = 0;
+  /**
+   * @brief Write the model's parameters to a file
+   *
+   * @param filename The file to write the model's parameter
+   */
+  virtual void save_paramters(const char *filename) const = 0;
 };
 
 #endif
--- include/cpu/cpu_autoencoder.h
+++ include/cpu/cpu_autoencoder.h
@@ -8,104 +8,168 @@
  *
  */
 class Cpu_Autoencoder : IAutoencoder {
-    /**
-     * @brief Encode while saving outputs for training
-     *
-     * @param dataset The dataset to encode
-     * @return Dataset The encoded dataset
-     */
-    Dataset _encode_save_output(const Dataset &dataset);
-    
-    /**
-     * @brief Decode while saving outputs for training
-     *
-     * @param dataset The dataset to decode
-     * @return Dataset The decoded dataset
-     */
-    Dataset _decode_save_output(const Dataset &dataset);
 
-    /**
-     * @brief Allocate memory for the output arrays
-     *
-     * @param n The number of images
-     * @param width The width of the images
-     * @param height The height of the images
-     *
-     */
-    void _allocate_output_mem(int n, int width, int height);
+  unique_ptr<float[]> _encoder_filter_1;
+  unique_ptr<float[]> _encoder_bias_1;
 
-    /**
-     * @brief Deallocate memory by the output arrays
-     *
-     */
-    void _deallocate_output_mem();
+  // Save output for backwards propogation
+  unique_ptr<float[]> _out_encoder_filter_1;
+  unique_ptr<float[]> _out_encoder_bias_1;
 
-    /**
-     * @brief Fit a minibatch to the model
-     *
-     * @param batch The batch to fit
-     * @param learning_rate The learning rate
-     * @return float Loss of the minibatch
-     */
-    float _fit_batch(const Dataset &batch, float learning_rate);
+  unique_ptr<float[]> _out_encoder_relu_1;
+  unique_ptr<float[]> _out_max_pooling_1;
 
-  public:
-    /**
-     * @brief Initialize a new autoencoder with random paramters
-     *
-     */
-    Cpu_Autoencoder();
+  unique_ptr<float[]> _encoder_filter_2;
+  unique_ptr<float[]> _encoder_bias_2;
+  unique_ptr<float[]> _out_encoder_filter_2;
+  unique_ptr<float[]> _out_encoder_bias_2;
 
-    /**
-     * @brief Reads the parameters from a file
-     *
-     * @param filename The file containing the model's parameters
-     */
-    Cpu_Autoencoder(const char *filename);
+  unique_ptr<float[]> _out_encoder_relu_2;
+  unique_ptr<float[]> _out_max_pooling_2;
 
-    /**
-     * @brief Destroy the Autoencoder object
-     *
-     */
-    ~Cpu_Autoencoder() override = default;
+  unique_ptr<float[]> _decoder_filter_1;
+  unique_ptr<float[]> _decoder_bias_1;
+  unique_ptr<float[]> _out_decoder_filter_1;
+  unique_ptr<float[]> _out_decoder_bias_1;
 
-    /**
-     * @brief Encodes a dataset
-     *
-     * @param dataset The dataset to be encoded
-     * @return Dataset The encoded dataset
-     */
-    Dataset encode(const Dataset &dataset) const override;
+  unique_ptr<float[]> _out_decoder_relu_1;
+  unique_ptr<float[]> _out_upsampling_1;
 
-    /**
-     * @brief Decodes a dataset
-     *
-     * @param dataset The dataset to be decoded
-     * @return Dataset The decoded dataset
-     */
-    Dataset decode(const Dataset &dataset) const override;
+  unique_ptr<float[]> _decoder_filter_2;
+  unique_ptr<float[]> _decoder_bias_2;
+  unique_ptr<float[]> _out_decoder_filter_2;
+  unique_ptr<float[]> _out_decoder_bias_2;
 
-    /**
-     * @brief Trains the model using a dataset
-     *
-     * @param dataset The dataset
-     * @param n_epoch The number of epochs
-     * @param batch_size Minibatch size (set to 0 to disable)
-     * @param learning_rate Learning rate of the model
-     * @param verbose Whether to disable more or less information
-     * @param checkpoint Save the model's parameter after a specific number of epochs (set to 0 to disable)
-     * @param output_dir The file to save model's param
-     */
-    void fit(const Dataset &dataset, int n_epoch, int batch_size, float learning_rate,
-            bool verbose, int checkpoint, const char *output_dir = "./model") override;
+  unique_ptr<float[]> _out_decoder_relu_2;
+  unique_ptr<float[]> _out_upsampling_2;
 
-    /**
-     * @brief Evaluate the model
-     *
-     * @param dataset The dataset to be evaluated
-     * @return float The MSE between the actual and expected result
-     */
-    float eval(const Dataset &dataset) override;
+  unique_ptr<float[]> _decoder_filter_3;
+  unique_ptr<float[]> _decoder_bias_3;
+  unique_ptr<float[]> _out_decoder_filter_3;
+  unique_ptr<float[]> _out_decoder_bias_3;
+
+  // Gradients
+  unique_ptr<float[]> _d_in;
+  unique_ptr<float[]> _d_out;
+  unique_ptr<float[]> _d_filter;
+
+  /**
+   * @brief Encode while saving outputs for training
+   *
+   * @param dataset The dataset to encode
+   * @return Dataset The encoded dataset
+   */
+  Dataset _encode_save_output(const Dataset &dataset);
+
+  /**
+   * @brief Decode while saving outputs for training
+   *
+   * @param dataset The dataset to decode
+   * @return Dataset The decoded dataset
+   */
+  Dataset _decode_save_output(const Dataset &dataset);
+
+  /**
+   * @brief Allocate memory for the parameters
+   *
+   */
+  void _allocate_mem();
+
+  /**
+   * @brief Allocate memory for the output arrays
+   *
+   * @param n The number of images
+   * @param width The width of the images
+   * @param height The height of the images
+   *
+   */
+  void _allocate_output_mem(int n, int width, int height);
+
+  /**
+   * @brief Deallocate memory by the output arrays
+   *
+   */
+  void _deallocate_output_mem();
+
+  /**
+   * @brief Fit a minibatch to the model
+   *
+   * @param batch The batch to fit
+   * @param learning_rate The learning rate
+   * @return float Loss of the minibatch
+   */
+  float _fit_batch(const Dataset &batch, float learning_rate);
+
+public:
+  /**
+   * @brief Initialize a new autoencoder with random paramters
+   *
+   */
+  Cpu_Autoencoder();
+
+  /**
+   * @brief Reads the parameters from a file
+   *
+   * @param filename The file containing the model's parameters
+   */
+  Cpu_Autoencoder(const char *filename);
+
+  /**
+   * @brief Destroy the Autoencoder object
+   *
+   */
+  ~Cpu_Autoencoder() override = default;
+
+  /**
+   * @brief Encodes a dataset
+   *
+   * @param dataset The dataset to be encoded
+   * @return Dataset The encoded dataset
+   */
+  Dataset encode(const Dataset &dataset) const override;
+
+  /**
+   * @brief Decodes a dataset
+   *
+   * @param dataset The dataset to be decoded
+   * @return Dataset The decoded dataset
+   */
+  Dataset decode(const Dataset &dataset) const override;
+
+  /**
+   * @brief Trains the model using a dataset
+   *
+   * @param dataset The dataset
+   * @param n_epoch The number of epochs
+   * @param batch_size Minibatch size (set to 0 to disable)
+   * @param learning_rate Learning rate of the model
+   * @param verbose Whether to disable more or less information
+   * @param checkpoint Save the model's parameter after a specific number of epochs (set
+   * to 0 to disable)
+   * @param output_dir The file to save model's param
+   */
+  void fit(const Dataset &dataset,
+           int            n_epoch,
+           int            batch_size,
+           float          learning_rate,
+           bool           verbose,
+           int            checkpoint,
+           const char    *output_dir = "./model") override;
+
+  /**
+   * @brief Evaluate the model
+   *
+   * @param dataset The dataset to be evaluated
+   * @return float The MSE between the actual and expected result
+   */
+  float eval(const Dataset &dataset) override;
+
+  /**
+   * @brief Write the model's parameters to a file
+   *
+   * @param filename The file to write the model's parameter
+   */
+  void save_paramters(const char *filename) const override;
 };
 
 #endif

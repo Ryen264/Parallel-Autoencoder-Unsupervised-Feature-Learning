6d52989c3b2dc05cf81d18d1a41241e946a276f4
LilLongg <glivingstone70@gmail.com>
2025-12-17 13:57:40 +0700

    Update gpu to use constants.h

--- src/progress_bar.cu
+++ src/progress_bar.cu
@@ -14,21 +14,21 @@ Progress_Bar::Progress_Bar(int step, const string &text, int size)
 void Progress_Bar::update() {
   if (_cur_step == _step) {
     printf(
-        "\r%s %d/%d [%s] (100%%)", _text.c_str(), _cur_step, _step, _progress.c_str());
+        "\r%s %d/%d (100%%) [%s]", _text.c_str(), _cur_step, _step, _progress.c_str());
   } else {
     int percentage = round(100.0f * _cur_step / _step);
     int progress   = _cur_step * _size / _step;
-    printf("\r%s %0*d/%d [%.*s%c%.*s] (%02d%%)",
+    printf("\r%s %0*d/%d (%02d%%) [%.*s%c%.*s]",
            _text.c_str(),
            _number_padding,
            _cur_step,
            _step,
+           percentage,
            progress,
            _progress.c_str(),
            _CUR_CHAR,
            _size - progress - 1,
-           _padding.c_str(),
-           percentage);
+           _padding.c_str());
 
     ++_cur_step;
   }
